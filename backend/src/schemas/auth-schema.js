86abd8cee1855e5312e2f46fc5f3ab0809b1a8ca
balumartin <martinka.balazs@gmail.com>
2024-09-24 15:19:07 +0200

    Add yup validation schemas for user registration and login

---
+++ backend/src/schemas/auth-schema.js
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+
+export const registerSchema = yup.object().shape({
+  userName: yup.string().required("User name is required"),
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .required("Password is required"),
+});
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
