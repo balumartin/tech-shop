69e22583d36967776d59303259f525a4d95d758f
balumartin <martinka.balazs@gmail.com>
2024-09-24 15:25:32 +0200

    Configure Express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import cors from "cors";
+import apiRoutes from "./routes/auth-routes.js"
+import errorHandler from "./middlewares/error-handler-middleware.js";
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/auth", apiRoutes)
+
+apiRoutes.use(errorHandler);
+export default app;
