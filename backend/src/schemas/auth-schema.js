b11cd6cf500c8d8eb5b5fbedc0df47400d5d5cec
balumartin <martinka.balazs@gmail.com>
2024-09-30 13:05:13 +0200

    Fix auth-schema with confirm password

--- backend/src/schemas/auth-schema.js
+++ backend/src/schemas/auth-schema.js
@@ -8,7 +8,15 @@ export const registerSchema = yup.object().shape({
     .required("Email is required"),
   password: yup
     .string()
-    .min(8, "Password must be at least 8 characters long")
+    .min(8, "Password must be 8 characters long")
+    .matches(/[0-9]/, "Password requires a number")
+    .matches(/[a-z]/, "Password requires a lowercase letter")
+    .matches(/[A-Z]/, "Password requires an uppercase letter")
+    .matches(/[#?!@$%^&*-]/, "Password requires a symbol")
+    .required("Password is required"),
+  confirm: yup
+    .string()
+    .oneOf([yup.ref("password"), null], 'Must match "password" field value')
     .required("Password is required"),
 });
 
